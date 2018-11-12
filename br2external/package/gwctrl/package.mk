733a55486c41603658bae926f49204a90a00afbd
Daniel Palmer <daniel@0x0f.com>
2018-11-12 23:28:38 +0900

    Install gwctrl init script properly

--- br2external/package/gwctrl/package.mk
+++ br2external/package/gwctrl/package.mk
@@ -4,4 +4,8 @@ GWCTRL_SITE_METHOD = git
 GWCTRL_DEPENDENCIES = host-pkgconf #glib-networking
 GWCTRL_GIT_SUBMODULES = YES
 
+define GWCTRL_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 $(GWCTRL_PKGDIR)/S58gwctrl $(TARGET_DIR)/etc/init.d/S58gwctrl
+endef
+
 $(eval $(meson-package))
