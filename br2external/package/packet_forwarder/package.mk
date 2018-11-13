4618f270f507512655c96b4c094bd9fd7076e2dd
Daniel Palmer <daniel@0x0f.com>
2018-11-13 20:55:47 +0900

    Install packer forwarder init script properly

--- br2external/package/packet_forwarder/package.mk
+++ br2external/package/packet_forwarder/package.mk
@@ -11,4 +11,8 @@ define PACKET_FORWARDER_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/lora_pkt_fwd/lora_pkt_fwd $(TARGET_DIR)/usr/bin/lora_pkt_fwd
 endef
 
+define PACKET_FORWARDER_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 $(PACKET_FORWARDER_PKGDIR)/S60pktfwd $(TARGET_DIR)/etc/init.d/S60pktfwd
+endef
+
 $(eval $(generic-package))
