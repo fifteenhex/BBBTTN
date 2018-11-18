d57dedf7e6e54230aec93f3ff69519fdd26675a2
Daniel Palmer <daniel@0x0f.com>
2018-11-18 16:17:37 +0900

    Misc build fixes

--- br2external/package/pktfwdbr/package.mk
+++ br2external/package/pktfwdbr/package.mk
@@ -4,16 +4,8 @@ PKTFWDBR_SITE_METHOD = git
 PKTFWDBR_DEPENDENCIES = host-pkgconf #glib-networking
 PKTFWDBR_GIT_SUBMODULES = YES
 
-define PKTFWDBR_BUILD_CMDS
-	PKGCONFIG=$(PKG_CONFIG_HOST_BINARY) CC=$(TARGET_CROSS)gcc $(MAKE) -C $(@D)
-endef
-
-define PKTFWDBR_INSTALL_TARGET_CMDS
-        $(INSTALL) -D -m 0755 $(@D)/pktfwdbr $(TARGET_DIR)/usr/bin/pktfwdbr
-endef
-
 define PKTFWDBR_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 $(PKTFWDBR_PKGDIR)/S59pktfwdbr $(TARGET_DIR)/etc/init.d/S59pktfwdbr
 endef
 
-$(eval $(generic-package))
+$(eval $(meson-package))
