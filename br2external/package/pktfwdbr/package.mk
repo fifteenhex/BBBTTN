88b1c8b8966b600e50c9d2924e65b2b55cf9498e
Daniel Palmer <daniel@0x0f.com>
2019-01-31 20:55:52 +0900

    Fix hard coding in pktfwdbr too.

--- br2external/package/pktfwdbr/package.mk
+++ br2external/package/pktfwdbr/package.mk
@@ -5,7 +5,10 @@ PKTFWDBR_DEPENDENCIES = host-pkgconf glib-networking
 PKTFWDBR_GIT_SUBMODULES = YES
 
 define PKTFWDBR_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(PKTFWDBR_PKGDIR)/S59pktfwdbr $(TARGET_DIR)/etc/init.d/S59pktfwdbr
+	sed -e s#TMPL_MQTT_HOST#$(BR2_BBLWGW_MQTT_HOST)# \
+		$(PKTFWDBR_PKGDIR)/S59pktfwdbr.template > \
+		$(TARGET_DIR)/etc/init.d/S59pktfwdbr
+	chmod 755 $(TARGET_DIR)/etc/init.d/S59pktfwdbr
 endef
 
 $(eval $(meson-package))
